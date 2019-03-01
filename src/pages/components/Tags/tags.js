841837955ca227e5ade1e5b399db9983a8cfdc21
aditya <aditya.ramariya@gmail.com>
2019-03-02 01:12:21 +0530

    initial commit

--- src/pages/components/Tags/tags.js
+++ src/pages/components/Tags/tags.js
@@ -9,12 +9,9 @@ class Tags extends Component {
                         Tag label
                     <button className="delete is-small"></button>
                 </span>
-
-
                 <span className="tag is-tagged">
                         Tag label
                 </span>
-
 
                 <span className="tag is--default">
                         Default Tags
